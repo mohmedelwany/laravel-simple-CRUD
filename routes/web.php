171dd0f3bfdd7613c2d6e187d1b089dbb6d1089f
mohmedelwany <mohmedelwany2@gmail.com>
2022-02-16 17:19:03 +0200

    create Device(resource controller, model, migration)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\http\Controllers\DeviceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource("device", DeviceController::class);
