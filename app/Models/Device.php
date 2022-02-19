fc8228b8a4e344726566823186ccc5772940d4df
mohmedelwany <mohmedelwany2@gmail.com>
2022-02-19 16:53:36 +0200

    using Model life cycle functions to log CRUD

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -4,9 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class Device extends Model
 {
     use HasFactory;
     public $timestamps = false;
+
+    public static function boot() {
+        parent::boot();
+
+        static::creating(function ($item) {
+            Log::info("creating event fired ". $item);
+        });
+
+        static::created(function ($item) {
+            Log::info("Created event fired ". $item);
+        });
+
+        static::updating(function ($item) {
+            Log::info("Updating event fired ". $item);
+        });
+
+        static::updated(function ($item) {
+            Log::info("Updated event fired ". $item );
+        });
+
+        static::deleting(function ($item) {
+            Log::info("Deleting event fired ". $item);
+        });
+
+        static::deleted(function ($item) {
+            Log::info("Deleted event fired ". $item);
+        });
+    }
 }
